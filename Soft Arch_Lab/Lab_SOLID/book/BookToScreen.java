796497d10d33b155e6f9e2f82df64dd6379b9bf7
Tnp_Sr <69105766+Tnp-Sr@users.noreply.github.com>
2022-10-17 13:45:47 +0700

    Lab SOLID

---
+++ Soft Arch_Lab/Lab_SOLID/book/BookToScreen.java
@@ -0,0 +1,19 @@
+
+
+import java.util.List;
+
+
+public class BookToScreen extends Book{
+    
+    public BookToScreen(String title, List<String> pages) {
+        super(title, pages);
+    }
+    
+    public void printToScreen() {
+        Book book = this;
+        do {
+            System.out.println(book.getCurrentPage());
+        } while (book.turnToNextPage());
+    }
+    
+}
